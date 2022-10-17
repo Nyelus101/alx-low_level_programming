db3140ae6ba881fb55676024eaa7b001714841fa
Nyelus101 <cmuonyelu@gmail.com>
2022-10-17 13:53:09 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+*main - entry point
+*
+*Description: use printf function to return a string
+*
+*Return: always 0 (sucess)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
