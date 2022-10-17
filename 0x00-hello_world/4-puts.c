406d92629ceafe0653dd19ab370cbf37ce687fd7
Nyelus101 <cmuonyelu@gmail.com>
2022-10-17 13:09:38 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - entry point
+*
+*Description: use put function to return a string
+*
+*Return: always 0 (sucess)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
