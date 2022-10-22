83173051b7f4134c00914079559b520d2fbea485
Nyelus101 <cmuonyelu@gmail.com>
2022-10-22 04:38:19 -0700

    When I was having that alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: conditional statements
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x == 'e' || x == 'q')
+			continue;
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
