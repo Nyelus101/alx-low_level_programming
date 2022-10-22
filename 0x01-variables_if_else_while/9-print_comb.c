6619d7389ed71b2964e24afbb7c9f0fbfd29acc1
Nyelus101 <cmuonyelu@gmail.com>
2022-10-22 07:57:35 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: conditional statements
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+		printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
