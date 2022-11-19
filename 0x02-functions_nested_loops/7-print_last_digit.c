f7e3f1e858f4252d3c3b25bebb5a9639939a01cf
Nyelus101 <cmuonyelu@gmail.com>
2022-11-18 16:21:17 -0800

    print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ *
+ * Description: checks if parameter is alphabet
+ *
+ * @c: is the expected parameter
+ *
+ * Return: value of the last digit
+ */
+int print_last_digit(int c)
+{
+	int s;
+
+	s = (c % 10);
+
+	if (s < 0)
+	{
+		s = (-1 * s);
+	}
+	_putchar(s + '0');
+	return (s);
+}
